a2171709bc010e58c350765bc1ec1d30bee67079
aleks-rosca <56295581+aleks-rosca@users.noreply.github.com>
2021-05-18 13:16:01 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
     console.log('new connection')
-    res.end('Test project')
+    res.end('Test project with continuous updates')
 })
 const PORT = process.env.PORT || 8080
 server.listen(PORT, () => console.log('Listening'))
